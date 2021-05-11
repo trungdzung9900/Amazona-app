8c2a73a7e4b423e32c3f2b8f9c161c4cc424abad
DungNT <trungdung.nguyen@vmodev.com>
2021-05-11 09:12:03 +0700

    Share code on git hub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './index.css';
+import React from 'react';
+import data from './data';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">amazona</a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign in</a>
+        </div>
       </header>
+      <main>
+        <div className="row center">
+          {
+            data.product.map((product =>
+              <div key={product._id} className="card">
+                <a href={`/product/${product._id}`}>
+                  <img className="medium-image" src={product.image} alt="product" />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                    <h2>
+                      {product.name}
+                    </h2>
+                  </a>
+                  <div className="raiting">
+                    <span>
+                      <i className="fas fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fas fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fas fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fas fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fas fa-star-half"></i>
+                    </span>
+                  </div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </div>
+            ))
+          }
+
+        </div>
+      </main>
+      <footer className="row center">
+        All right reserved
+        </footer>
     </div>
   );
 }
